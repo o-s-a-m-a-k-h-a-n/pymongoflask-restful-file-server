a05322eac864fd0027b644c4800b43bed415427d
Osama Khan <osamakhn@gmail.com>
2016-04-24 23:36:46 -0700

    complete read/write of user files only with basic unsecured auth

--- app.py
+++ app.py
@@ -32,17 +32,14 @@ MAX_FILE_SIZE = db.configs.find_one({"name":"max_file_size"})['max_file_size'] i
 @auth.verify_password
 def verify_password(username, password):
     # as this is the most secure fileserver in the world, we will just retrieve the
-    # username and password information from db, if exists, and verify user. Yay!
+    # username and password information from db, if exists, and verify user; yayy!
     record = db.users.find_one({"username":username})
     if record is not None:
         if record['username'] == username and record['password'] == password:
-            print "VERIFIED"
+            g.username = username
             return True
         else:
-            print "Unauthorized Access"
             return False
-
-
 
 ##########
 # routes #
@@ -54,48 +51,53 @@ def users():
     return json.dumps({'results': MAX_FILE_SIZE}),200
 
 # register new user
-# NOTE: not hashing or securing password to develop contrived app for demo/text purposes
+# NOTE: not hashing or securing password to develop contrived app for demo/test purposes
 @app.route('/users', methods=['POST'])
 def new_user():
     username = request.args.get('username')
     password = request.args.get('password')
     if username is None or password is None:
         abort(400) # missing arguments
-    if db.users.find({"username":username}).count() > 0:
-        abort(400) # existing user
-    new_user = db.users.insert({'username':username, 'password':password})
-    if db.users.find_one(new_user) is not None:
-        return jsonify({'status':'New user created successfully'}), 201
-    else:
-        return jsonify({'status': 'Error ocurred during user creation'})
+        if db.users.find({"username":username}).count() > 0:
+            abort(400) # existing user
+            new_user = db.users.insert({'username':username, 'password':password})
+            if db.users.find_one(new_user) is not None:
+                return jsonify({'status':'New user created successfully'}), 201
+            else:
+                return jsonify({'status': 'Error ocurred during user creation'}), 500
 
 
 # upload route expecting a filename in route and file contents in data
 @app.route('/upload/<file_name>', methods=['POST'])
 @auth.login_required
 def upload(file_name):
-    # TODO: link each file to user
     uploaded_file = request.data
     if len(uploaded_file) < MAX_FILE_SIZE:
+        user_record = db.users.find_one({"username":g.username})
         with gfs.new_file(filename=file_name) as fp:
             file_id = fp.write(uploaded_file)
             if gfs.find_one(file_id) is not None:
+                db.users.update({'username':g.username}, {'$set': {file_name:fp._id}}) # store different file types with the same name
                 return jsonify({'status': 'File saved successfully'}), 200
             else:
                 return jsonify({'status': 'Error occured while saving file'}), 500
     else:
-        return jsonify({'status': 'File exceeded allowed size limit'}), 500
+        return jsonify({'status': 'File exceeded allowed size limit'}), 400
 
 # download route returns contents for given filename if exists in gfs
 @app.route('/download/<file_name>')
 @auth.login_required
 def download(file_name):
-    # TODO: get the username, check if the user object has such a file and return accordingly
-    gfs_file = gfs.find_one({'filename': file_name})
-    response = make_response(gfs_file.read())
-    response.headers['Content-Type'] = 'application/octet-stream'
-    response.headers["Content-Disposition"] = "attachment; filename={}".format(file_name)
-    return response
+    try:
+        user_record = db.users.find_one({'username':g.username})
+        filename, extension = file_name.split('.')[0], file_name.split('.')[1]
+        gfs_file = gfs.find_one({'_id': user_record[filename][extension]})
+        response = make_response(gfs_file.read())
+        response.headers['Content-Type'] = 'application/octet-stream'
+        response.headers["Content-Disposition"] = "attachment; filename={}".format(file_name)
+        return response
+    except:
+        abort(400)
 
 ########
 # main #
