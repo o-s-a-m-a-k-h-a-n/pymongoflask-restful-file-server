277d17fc83df35219cbe7433cbb6687b81c8f51a
Osama Khan <osamakhn@gmail.com>
2016-04-25 00:40:48 -0700

    clean up code for some edgecases

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# PyGFuSsyMON
+# PyGFSTPM
 
 > RESTful API written using Python Flask to enable users to register and then upload/download their files to a MongoDB's GridFS
+
+### RESTful Endpoints
+
+* ```POST /users?username=<new-username>&password=<new-password>``` *~>* register new user
+
+* ```POST /upload/<filename>``` + (request.data contains file-contents) *~>* upload new file as authenticated user
+
+* ```POST /configs?max_file_size=<max_size_allowed>``` *~>* admin_only endpoint to update upload limit
+
+_Assumes MongoDB instance running on default port/settings locally without auth._
+
+#### Default Admin Credentials:
+* username: ```admin```
+* password: ```admin```
+
+_More users can be made admin through record update in MongoDB_
--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 ###########
 # imports #
 ###########
-import json
+# import json
 from flask import Flask, request, make_response, abort, jsonify, g
 from flask.ext.httpauth import HTTPBasicAuth
 from pymongo import MongoClient
@@ -22,7 +22,6 @@ auth = HTTPBasicAuth()
 # gfs_filestore.configs collection is assume to have one document 'max_file_size'
 # with the maximum upload file size limit set by admin
 # if document is not configured properly, default max size to 50MB
-# TODO: create an admin endpoint to basic authenticate and change this value
 MAX_FILE_SIZE = db.configs.find_one({"name":"max_file_size"})['max_file_size'] if db.configs.find_one({"name":"max_file_size"}) else 50000000
 
 #############################
@@ -34,21 +33,24 @@ def verify_password(username, password):
     # as this is the most secure fileserver in the world, we will just retrieve the
     # username and password information from db, if exists, and verify user; yayy!
     record = db.users.find_one({"username":username})
-    if record is not None:
-        if record['username'] == username and record['password'] == password:
-            g.username = username
-            return True
-        else:
-            return False
+    if record is not None and record['username'] == username and record['password'] == password:
+        g.username = username
+        g.admin = record['admin'] or False
+        return True
+    else:
+        return False
 
 ##########
 # routes #
 ##########
-@app.route('/users', methods=['GET'])
+@app.route('/configs', methods=['POST'])
 @auth.login_required
-def users():
-    user_list = db.users.find().count()
-    return json.dumps({'results': MAX_FILE_SIZE}),200
+def set_max():
+    if g.admin:
+        db.configs.update({"name":"max_file_size"},{'$set':{'max_file_size':request.args.get('max_file_size')}})
+        return jsonify({'status': 'New maximum file size limit set'}),200
+    else:
+        return jsonify({'status':'You are not authorized to set configs.'}), 403
 
 # register new user
 # NOTE: not hashing or securing password to develop contrived app for demo/test purposes
@@ -57,14 +59,14 @@ def new_user():
     username = request.args.get('username')
     password = request.args.get('password')
     if username is None or password is None:
-        abort(400) # missing arguments
-        if db.users.find({"username":username}).count() > 0:
-            abort(400) # existing user
-            new_user = db.users.insert({'username':username, 'password':password})
-            if db.users.find_one(new_user) is not None:
-                return jsonify({'status':'New user created successfully'}), 201
-            else:
-                return jsonify({'status': 'Error ocurred during user creation'}), 500
+        return jsonify({'status': 'Username and Password information not passed for new user.'}), 400 # missing arguments
+    if db.users.find({"username":username}).count() > 0:
+        return jsonify({'status': 'Existing user. Please select other username.'}), 400 # existing user
+    new_user = db.users.insert({'username':username, 'password':password, 'admin':False})
+    if db.users.find_one(new_user) is not None:
+        return jsonify({'status':'New user created successfully'}), 201
+    else:
+        return jsonify({'status': 'Error ocurred during user creation'}), 500
 
 
 # upload route expecting a filename in route and file contents in data
@@ -90,17 +92,24 @@ def upload(file_name):
 def download(file_name):
     try:
         user_record = db.users.find_one({'username':g.username})
-        filename, extension = file_name.split('.')[0], file_name.split('.')[1]
-        gfs_file = gfs.find_one({'_id': user_record[filename][extension]})
+        if "." in file_name:
+            filename, extension = file_name.split('.')[0], file_name.split('.')[1]
+            gfs_file = gfs.find_one({'_id': user_record[filename][extension]})
+        else:
+            gfs_file = gfs.find_one({'_id':user_record[file_name]})
         response = make_response(gfs_file.read())
         response.headers['Content-Type'] = 'application/octet-stream'
         response.headers["Content-Disposition"] = "attachment; filename={}".format(file_name)
         return response
     except:
-        abort(400)
+        return jsonify({'status': 'File does not exist or you are not authorized to access this file.'}), 403
 
 ########
 # main #
 ########
 if __name__ == '__main__':
+    # create admin user and prompt credentials to console
+    if db.users.find_one({'username':'admin'}) is None:
+        db.users.insert({'username':'admin', 'password':'admin','admin':True})
+    print "* Administrator Username: 'admin' Password: 'admin'"
     app.run(debug=True)
